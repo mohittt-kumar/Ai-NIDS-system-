b6ee130c58b803f65a6e1c8aaece7e6b3704b1cb
Security Analyst <analyst@ainids.local>
2026-07-27 11:38:51 +0530

    Add Vercel deployment configuration and route SQLite to /tmp

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'security-operations-center-nids-key-987654'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'nids.db')
+    if os.environ.get('VERCEL') == '1':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/nids.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'instance', 'nids.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Path mappings
